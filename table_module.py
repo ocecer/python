d857d23351d8f9c4c388e87c4898e07e1784d607
ocecer <54540749+ocecer@users.noreply.github.com>
2020-06-25 12:44:12 +0300

    Update table_module.py

--- table_module.py
+++ table_module.py
@@ -13,110 +13,110 @@ url='http://lotobayisi.com/Sayisal-Butun-Veriler.aspx'
 #url='http://lotobayisi.com/On-Numara-Butun-Veriler.aspx'
                 
 # Create a page, to handle the contents of the website
-page_sayisal = requests.get(url)
+page = requests.get(url)
 
 #Store the contents of the website under doc
-doc_sayisal = lh.fromstring(page_sayisal.content)
+doc = lh.fromstring(page.content)
 
 #Parse data that are stored between <tr>..</tr> of HTML
-tr_elements_sayisal = doc_sayisal.xpath('//tr')
+tr_elements = doc.xpath('//tr')
 
 
 #Check first 12 rows for Sayısal Loto
 print("Sayısal Loto satır kontrol: ")
-[len(T_sayisal) for T_sayisal in tr_elements_sayisal[:12]]
+[len(T) for T in tr_elements[:12]]
 
 
-tr_elements_sayisal = doc_sayisal.xpath('//tr')
+tr_elements = doc.xpath('//tr')
 
 #Create empty list
-col_sayisal=[]
+col=[]
 i=0
 
 #For each row, store each first element (header) and an empty list
-for t_sayisal in tr_elements_sayisal[0]:
+for t in tr_elements[0]:
     i+=1
-    name_sayisal=t_sayisal.text_content()
-    print('%d:"%s"'%(i,name_sayisal))
-    col_sayisal.append((name_sayisal,[]))
+    name=t.text_content()
+    print('%d:"%s"'%(i,name))
+    col.append((name,[]))
 
 
 #Since out first row is the header, data is stored on the second row onwards
-for j_sayisal in range(1,len(tr_elements_sayisal)):
+for j in range(1,len(tr_elements)):
     #T is our j'th row
-    T_sayisal=tr_elements_sayisal[j_sayisal]
+    T=tr_elements[j]
     
     #If row is not of size 8, the //tr data is not from our table 
-    if len(T_sayisal)!=8:
+    if len(T)!=8:
         break
         
     #i is the index of our column
     i=0
     
     #Iterate through each element of the row
-    for t_sayisal in T_sayisal.iterchildren():
-        data_sayisal=t_sayisal.text_content() 
+    for t in T.iterchildren():
+        data=t.text_content() 
         
         #Check if row is empty
         if i>0:
      
         #Convert any numerical value to integers
             try:
-                data_sayisal=int(data_sayisal)
+                data=int(data)
             except:
                 pass
             
         #Append the data to the empty list of the i'th column
-        col_sayisal[i][1].append(data_sayisal)
+        col[i][1].append(data)
         #Increment i for the next column
         i+=1
 
 
 print("Sayısal Loto data kontrol - 1")
-[len(C_sayisal) for (title,C_sayisal) in col_sayisal]
+[len(C) for (title,C) in col]
 
 
 #Create Sayısal Loto dataframe
-Dict_sayisal={title:column for (title,column) in col_sayisal}
-df_sayisal=pd.DataFrame(Dict_sayisal)
+Dict={title:column for (title,column) in col}
+df=pd.DataFrame(Dict)
 
 
-df_sayisal.head()
+df.head()
 
 
-len(df_sayisal)
+len(df)
 
 
 #Change column names
-df_sayisal.columns=['Hafta', 'Tarih', 'Sayi1', 'Sayi2', 'Sayi3', 'Sayi4', 'Sayi5', 'Sayi6']
+df.columns=['Hafta', 'Tarih', 'Sayi1', 'Sayi2', 'Sayi3', 'Sayi4', 'Sayi5', 'Sayi6']
 
 #Remove \r\n from the table
-df_sayisal = df_sayisal.replace('\r\n','', regex=True)
-df_sayisal = df_sayisal.replace(' ','', regex=True)
+df = df.replace('\r\n','', regex=True)
+df = df.replace(' ','', regex=True)
 
-df_sayisal.head()
+df.head()
 
 
 #Drop duplicate weeks from the dataframe
-df_sayisal.drop_duplicates(subset='Hafta', inplace=True, keep='first')
+df.drop_duplicates(subset='Hafta', inplace=True, keep='first')
 
 
 print("Sayısal Loto data kontrol")
-len(df_sayisal)
+len(df)
 
 
-df_sayisal.head()
+df.head()
 
 
 #Create an empty list 
-row_list_sayisal =[] 
+row_list =[] 
   
 #Iterate over each row 
-for index, rows in df_sayisal.iterrows(): 
+for index, rows in df.iterrows(): 
     #Create list for the current row 
-    list_sayisal =[rows.Sayi1, rows.Sayi2, rows.Sayi3, rows.Sayi4, rows.Sayi5, rows.Sayi6] 
+    list =[rows.Sayi1, rows.Sayi2, rows.Sayi3, rows.Sayi4, rows.Sayi5, rows.Sayi6] 
     #Append the list to the final list 
-    row_list_sayisal.append(list_sayisal) 
+    row_list.append(list) 
   
 #Print the list 
-print(row_list_sayisal) 
+print(row_list) 
